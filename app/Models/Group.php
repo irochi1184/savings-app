15b23f8df221edce3281767cece14567d3bc03cf
irochi1184 <ken.office.arita@gmail.com>
2025-05-30 17:41:40 +0900

    モデル作成とリレーション定義

---
+++ app/Models/Group.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Group extends Model
+{
+    protected $fillable = ['name', 'invite_token'];
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function expenses(): HasMany
+    {
+        return $this->hasMany(Expense::class);
+    }
+
+    public function incomes(): HasMany
+    {
+        return $this->hasMany(Income::class);
+    }
+}
